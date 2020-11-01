529d0b868b21fff78cefa2584000b7f0276d5ed2
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-10-31 22:41:42 -0400

    EOD saturday

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,15 +3,12 @@ skip_before_action :authorized
 
      def index
           results = Movie.all
-
-          # results.each Movie.create 
-
          
           render json: results 
      end
 
      def show
-          single_movie = Api.single_movie(params[:id])
+          single_movie = Movie.find(params[:id])
      
           render json: single_movie
      end
--- app/controllers/my_movies_controller.rb
+++ app/controllers/my_movies_controller.rb
@@ -4,18 +4,9 @@ class MyMoviesController < ApplicationController
 def index 
      my_favorites = Favorite.where(user: current_user)
 
-#     movies = current_user.favorites.map{|movies| movies.title}
-#      if movies
           render json: my_favorites 
    
 end
 
-
-
-# def fave_movies 
-#      my_favorites.each do |movies|
-#           movies.title
-   
-# end
 
 end
