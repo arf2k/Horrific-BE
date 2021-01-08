fb4ee4ed7df0cf01b94d9a8c034369ec09a9e21e
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-01-08 16:55:54 -0500

    cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ skip_before_action :authorized, only: [:create, :login]
                token = encode_token({ user_id: @user.id})
                render json: { user: UserSerializer.new(@user), jwt: token}, status: :accepted
           else
-               render json: { message: 'Invalid username or password' }, status: :unauthorized
+               render json: { alert: 'Invalid username or password' }, status: :unauthorized
           end
      end
 
